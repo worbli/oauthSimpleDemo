9654196deeac74f9ee4751f666afe6c1785a6c64
Ivaylo Ushev <iushev@yahoo.com>
2019-09-25 08:26:45 +0300

    Initial commit

---
+++ src/routes/index.ts
@@ -0,0 +1,10 @@
+import { NextFunction, Request, Response, Router } from 'express';
+
+const router = Router();
+
+/* GET home page. */
+router.get('/', (req: Request, res: Response, next: NextFunction) => {
+  res.render('index', { title: 'Express' });
+});
+
+export default router;
---
+++ src/routes/users.ts
@@ -0,0 +1,10 @@
+import { NextFunction, Request, Response, Router } from 'express';
+
+const router = Router();
+
+/* GET users listing. */
+router.get('/', (req: Request, res: Response, next: NextFunction) => {
+  res.send('respond with a resource');
+});
+
+export default router;
