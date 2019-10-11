48cccea66c77e6e04bc75aa4ca10637449443404
Ivaylo Ushev <iushev@yahoo.com>
2019-10-11 18:05:00 +0300

    Use passport-worbli

--- src/passport.ts
+++ src/passport.ts
@@ -1,36 +1,19 @@
 import passport from "passport";
-import { Strategy as OAuth2Strategy, VerifyFunction, VerifyCallback } from "passport-oauth2";
-import axios from "axios";
+import { Strategy as WorbliStrategy, VerifyCallback } from "passport-worbli";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const verifyFunction: VerifyFunction = (accessToken: string, refreshToken: string, profile: any, cb: VerifyCallback) => {
-  axios.get(process.env.WORBLI_OAUTH2_ME, {
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-      Origin: 'http://localhost:3000/'
-    }
-  })
-  .then((response) => {
-    cb(null, {
-      ...response.data,
-      accessToken
-    });
-  })
-  .catch((error) => {
-    cb(error);
-  });
-}
-
-passport.use(new OAuth2Strategy({
-  authorizationURL: process.env.WORBLI_OAUTH2_AUTHORIZATION_URL,
-  tokenURL: process.env.WORBLI_OAUTH2_TOKEN_URL,
+passport.use(new WorbliStrategy({
   clientID: process.env.WORBLI_OAUTH2_CLIENT_ID,
   clientSecret: process.env.WORBLI_OAUTH2_CLIENT_SECRET,
   callbackURL: process.env.WORBLI_OAUTH2_CALLBACK_URL,
   scope: process.env.WORBLI_OAUTH2_SCOPE,
-}, verifyFunction));
+}, (accessToken: string, refreshToken: string, profile: any, cb: VerifyCallback) => {
+  // TODO: Save accessToken for future use
+  // TODO: Find or create local user
+  cb(null, profile);
+}));
 
 passport.serializeUser((user: any, done) => {
   done(null, JSON.stringify(user));
