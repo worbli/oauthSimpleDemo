221155dd1aec6756c4285c3172657d35abeebd37
Ivaylo Ushev <iushev@yahoo.com>
2019-10-11 19:20:01 +0300

    Exeute request with state

--- src/passport.ts
+++ src/passport.ts
@@ -9,6 +9,7 @@ passport.use(new WorbliStrategy({
   clientSecret: process.env.WORBLI_OAUTH2_CLIENT_SECRET,
   callbackURL: process.env.WORBLI_OAUTH2_CALLBACK_URL,
   scope: process.env.WORBLI_OAUTH2_SCOPE,
+  state: true,
 }, (accessToken: string, refreshToken: string, profile: any, cb: VerifyCallback) => {
   // TODO: Save accessToken for future use
   // TODO: Find or create local user
