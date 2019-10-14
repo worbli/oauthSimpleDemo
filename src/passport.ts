8da8d92aca7d37c01486b7312311fb45d00bd3c5
Ivaylo Ushev <iushev@yahoo.com>
2019-10-14 12:10:00 +0300

    Update callbackURLs

--- src/passport.ts
+++ src/passport.ts
@@ -12,7 +12,7 @@ passport.use(new OAuth2Strategy({
   tokenURL: process.env.WORBLI_OAUTH2_TOKEN_URL,
   clientID: process.env.WORBLI_OAUTH2_CLIENT_ID,
   clientSecret: process.env.WORBLI_OAUTH2_CLIENT_SECRET,
-  callbackURL: "http://127.0.0.1:3030/worbli/callback-oauth2",
+  callbackURL: `${process.env.WORBLI_OAUTH2_CALLBACK_URL}/callback-oauth2`,
   scope: process.env.WORBLI_OAUTH2_SCOPE,
 }, (accessToken: string, refreshToken: string, profile: any, cb: OAuth2VerifyCallback) => {
   // get worbli user info
@@ -23,6 +23,8 @@ passport.use(new OAuth2Strategy({
   })
   .then((response) => {
     // normalize user info to passport profile
+    // http://www.passportjs.org/docs/profile/
+    // https://tools.ietf.org/html/draft-smarr-vcarddav-portable-contacts-00
     var profile = {
       // id: String(response.data.id);
       displayName: [response.data.fname, response.data.mname, response.data.lname].join(" "),
@@ -53,9 +55,9 @@ passport.use(new OAuth2Strategy({
 passport.use(new WorbliStrategy({
   clientID: process.env.WORBLI_OAUTH2_CLIENT_ID,
   clientSecret: process.env.WORBLI_OAUTH2_CLIENT_SECRET,
-  callbackURL: "http://127.0.0.1:3030/worbli/callback-worbli",
+  callbackURL: `${process.env.WORBLI_OAUTH2_CALLBACK_URL}/callback-worbli`,
   scope: process.env.WORBLI_OAUTH2_SCOPE,
-  state: true,
+  state: true
 }, (accessToken: string, refreshToken: string, profile: any, cb: WorbliVerifyCallback) => {
   // TODO: Save accessToken for future use
   // TODO: Find or create local user
